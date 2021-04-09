2a434d0331e3d8c9113a81fcd412bc8ff39b2c82
Davi dos Santos <davi.slima75@gmail.com>
2021-04-09 16:39:53 -0300

    Reusing instantiation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -37,8 +37,6 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public Seller findById(Integer id) {
-		// # INSTANCIANDO UM DEPARTAMENTO E SETANDO OS VALORES DELE:
-		
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		try {
@@ -52,16 +50,8 @@ public class SellerDaoJDBC implements SellerDao {
 			rs = st.executeQuery();
 			
 			if (rs.next()) {
-				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName(rs.getString("DepName"));
-				Seller obj = new Seller();
-				obj.setId(rs.getInt("Id"));
-				obj.setName(rs.getString("Name"));
-				obj.setEmail(rs.getString("Email"));
-				obj.setBaseSalary(rs.getDouble("BaseSalary"));
-				obj.setBirthDate(rs.getDate("BirthDate"));
-				obj.setDepartment(dep);
+				Department dep = instatintiateDepartment(rs);
+				Seller obj = instantiateSeller(rs, dep);
 				
 				return obj;
 			}
@@ -76,6 +66,28 @@ public class SellerDaoJDBC implements SellerDao {
 			
 		}
 		
+	}
+	
+//	SET DA TABELA DE VENDEDORES:
+	
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller obj = new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep);
+		return obj;
+	}
+
+//	INSTANCIA DO DEPARTAMENTO: 
+
+	private Department instatintiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		return dep;
 	}
 
 	@Override
